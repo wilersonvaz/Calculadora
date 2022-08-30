c0927ee10d148a84a3b80be278304ecca9e76cd3
root <wilersonalves@gmail.com>
2022-08-30 20:25:41 -0300

    Second commit - Calculo de porcentagem funcionando

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.calculadora;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+
 public class MainActivity extends AppCompatActivity {
     String expressao = "";
     TextView resultado, idResultadoParcial;
+    int finalizado = 0;
+    private AlertDialog ajuda;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void ajuda() {
+        LayoutInflater li = getLayoutInflater();
+
+        //inflamos o layout alerta.xml na view
+        View view = li.inflate(R.layout.activity_ajuda, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ajuda");
+        builder.setView(view);
+        ajuda = builder.create();
+        ajuda.show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
@@ -43,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.calculadoraAvancada:
                 replaceFragment(new CalculadoraAvancada());
                 break;
+            case R.id.ajuda:
+                ajuda();
+                break;
+
         }
         return super.onOptionsItemSelected(item);
     }
@@ -56,15 +82,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
 
+        if(finalizado == 1){
+            resultado.setText("");
+            idResultadoParcial.setText(expressao="");
+            finalizado = 0;
+        }
+
         String numeroDigitado = ((TextView) view).getText().toString();
         resultado.setText("");
         System.out.println(numeroDigitado);
         if(numeroDigitado.equals("C")){
             idResultadoParcial.setText(expressao="");
-            resultado.setText("");
+
         }else if(numeroDigitado.equals("=") && !numeroDigitado.equals("")){
-            resultado.setText(Double.toString(calcula(expressao)));
-            expressao="";
+            if(expressao.contains("%")){
+                expressao = porcentagem(expressao);
+                System.out.println("Expressso: "+expressao);
+                resultado.setText(Double.toString(eval(expressao, getApplicationContext())));
+                expressao = expressao.replace("(", "");
+                expressao = expressao.replace("/100)*", "%");
+                System.out.println(expressao);
+            }else{
+                resultado.setText(Double.toString(eval(expressao, getApplicationContext())));
+            }
+
+            finalizado = 1;
+
+//            System.out.println(Double.toString(eval("((4 - 2^3 + 1) * -sqrt(3*3+4*4)) / 2", getApplicationContext())));
+
         }else{
             if(numeroDigitado.equals("X")){
                 expressao += "*";
@@ -72,14 +117,34 @@ public class MainActivity extends AppCompatActivity {
                 expressao += "/";
             }else if(numeroDigitado.equals("L")) {
                 expressao = expressao.substring(0, expressao.length()-1);
+            }else if(numeroDigitado.equals("√")) {
+                expressao += numeroDigitado.replace("√", "sqrt");
             }else{
                 expressao += numeroDigitado;
             }
-            idResultadoParcial.setText(expressao);
+
         }
+        idResultadoParcial.setText(expressao.replace("sqrt", "√"));
     }
 
-    public static double calcula(final String str) {
+    private String porcentagem(String numeroDigitado) {
+        String perc = "(";
+        try{
+            for (int i=0; i< numeroDigitado.length(); i++) {
+                char c = numeroDigitado.charAt(i);
+                if(c == '%'){
+                    perc+="/100)*";
+                }else{
+                    perc += c;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return perc;
+    }
+
+    public static double eval(final String str, Context applicationContext) {
         return new Object() {
             int pos = -1, ch;
 
@@ -99,7 +164,8 @@ public class MainActivity extends AppCompatActivity {
             double parse() {
                 nextChar();
                 double x = parseExpression();
-                if (pos < str.length()) throw new RuntimeException("Inesperado: " + (char)ch);
+//                if (pos < str.length())  throw new RuntimeException("Inesperado: " + (char)ch);
+                if (pos < str.length()) Toast.makeText(applicationContext, "Expressão mal formada, use o meu ajuda!", Toast.LENGTH_LONG).show();
                 return x;
             }
 
@@ -109,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
                     if      (eat('+')) x += parseTerm(); // Adiçao
                     else if (eat('-')) x -= parseTerm(); // subtraçao
                     else return x;
+                    System.out.println("x: "+x);
                 }
             }
 
@@ -125,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
                 if (eat('+')) return parseFactor(); //
                 if (eat('-')) return -parseFactor(); //
 
-                double x;
+                double x = 0;
                 int startPos = this.pos;
                 if (eat('(')) { // Parenteses
                     x = parseExpression();
@@ -141,9 +208,11 @@ public class MainActivity extends AppCompatActivity {
                     else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
                     else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
                     else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else throw new RuntimeException("Funcao desconhecida: " + func);
+                    else Toast.makeText(applicationContext, "Funcao desconhecida:"+ func, Toast.LENGTH_LONG).show();
+//                    else throw new RuntimeException("Funcao desconhecida: " + func);
                 } else {
-                    throw new RuntimeException("Inesperado: " + (char)ch);
+                    Toast.makeText(applicationContext, "Expressão mal formada, use o meu ajuda!", Toast.LENGTH_LONG).show();
+//                    throw new RuntimeException("Inesperado: " + (char)ch);
                 }
 
                 if (eat('^')) x = Math.pow(x, parseFactor()); // exponencial
